692a98799a555a0fa6cff15886eca45d07c18b0c
Tyler Dary <daryt@msn.com>
2015-01-16 15:46:43 +0000

    added unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,7 +8,20 @@ class LikesController < ApplicationController
       flash[:notice] = "Liked Bookmark"
       redirect_to [@bookmark]
     else
-      flash[:error] = "Unable to like bookmark.  Please try again"
+      flash[:error] = "Unable to like bookmark.  Please try again."
+      redirect_to [@bookmark]
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    like = current_user.likes.find(params[:id])
+
+    if like.destroy
+      flash[:notice] = "Unliked"
+      redirect_to [@bookmark]
+    else
+      flash[:error] = "Unable to unlike bookmark.  Please try again."
       redirect_to [@bookmark]
     end
   end
