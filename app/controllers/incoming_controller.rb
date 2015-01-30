c222b62395dc8bb8f39e9b8b72e87f8462b88d29
Tyler Dary <daryt@msn.com>
2015-01-30 21:50:48 +0000

    started working on mailgun integration

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,12 +5,20 @@ class IncomingController < ApplicationController
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
+    # puts "INCOMING PARAMS HERE: #{params}"
 
     # You put the message-splitting and business
     # magic here. 
 
     # Assuming all went well. 
+
+    tags = params['Subject'].split(',')
+    data = params['stripped-text'].split("\r\n")
+
+    @bookmark = Bookmark.create({name: data[0], url: data[1]})
+    @bookmark.topic_list.add(tags)
+    @bookmark.save()
+
     head 200
   end
 end
